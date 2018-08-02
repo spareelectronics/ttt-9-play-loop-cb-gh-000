a2869c4f127e445e5b7e2ab3a62f8f79dcb82070
Amin Ullah <aullah@umich.edu>
2018-08-02 23:40:39 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,12 +38,9 @@ end
 # Define your play method below
 
 def play(board)
-  counter = 0
-  loop do
-    counter += 1
+
+  9.times do
     turn(board)
-    if counter > 8
-      break
-    end
   end
+
 end
